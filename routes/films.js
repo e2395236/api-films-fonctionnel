b4f88d67940f0915d6fe846edf54edd72efa6dce
yungreggy <alexandre.gregoire.me@protonmail.com>
2024-03-30 15:44:58 -0400

    first commit

---
+++ routes/films.js
@@ -0,0 +1,164 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db.js");
+const auth = require("../middlewares/auth.js");
+const { check, validationResult } = require("express-validator");
+
+/**
+ * Cette route permet de récupérer la liste des films
+ * @route GET /films
+ */
+router.get(
+    "/liste-films",
+    [
+        check("limit").escape().trim().optional(true).isInt(),
+        check("orderDirection").escape().trim().isString().optional(true),
+        check("orderBy").escape().trim().isString().optional(true),
+    ],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+            const { orderBy = "titre", orderDirection = "asc", limit = 100 } = req.query;
+            const filmsRef = await db.collection("Films").orderBy(orderBy, orderDirection).limit(+limit).get();
+            const films = [];
+
+            filmsRef.forEach((doc) => {
+                films.push({ id: doc.id, ...doc.data() });
+            });
+
+            res.status(200).json(films);
+        } catch (e) {
+            res.status(500).json({ message: e.message });
+        }
+    }
+);
+
+/**
+ * Cette route permet de récupérer un film spécifique
+ * @route GET /films/{id}
+ */
+router.get("/film/:id", [check("id").escape().trim().notEmpty()], async (req, res) => {
+    try {
+        const validation = validationResult(req);
+        if (!validation.isEmpty()) {
+            return res.status(400).json({ message: "Données invalides" });
+        }
+
+        const id = req.params.id;
+        const filmRef = await db.collection("Films").doc(id).get();
+
+        if (!filmRef.exists) {
+            return res.status(404).json({ message: "Film non trouvé" });
+        }
+
+        res.status(200).json(filmRef.data());
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+/**
+ * Cette route protégée permet de créer un film
+ * @route POST /films
+ */
+router.post(
+    "/ajouter-film",
+    auth, 
+    [
+        check("titre").escape().trim().notEmpty().isString(),
+        check("genres").escape().trim().exists().isArray(),
+        check("description").escape().trim().notEmpty().isString(),
+        check("annee").escape().trim().notEmpty().isString(),
+        check("realisation").escape().trim().notEmpty().isString(),
+        check("titreVignette").escape().trim().notEmpty().isString(),
+      
+        // Ajoutez d'autres champs de validation au besoin
+    ],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+
+            const film = req.body;
+            const docRef = await db.collection("Films").add(film);
+
+            res.status(201).json({ id: docRef.id, ...film });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    }
+);
+
+/**
+ * Cette route protégée permet de modifier un film
+ * @route PUT /films/{id}
+ */
+router.put(
+    "/modifier-film/:id",
+    auth, 
+    [
+        check("id").escape().trim().notEmpty().isString(),
+        check("titre").escape().trim().notEmpty().isString(),
+        check("genres").escape().trim().exists().isArray(),
+        check("description").escape().trim().notEmpty().isString(),
+        check("annee").escape().trim().notEmpty().isString(),
+        check("realisation").escape().trim().notEmpty().isString(),
+        check("titreVignette").escape().trim().notEmpty().isString(),
+
+    
+        // Ajoutez d'autres champs de validation au besoin  
+      
+    ],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+
+            const id = req.params.id;
+            const updates = req.body;
+
+            await db.collection("Films").doc(id).update(updates);
+
+            res.status(200).json({ id, ...updates });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    }
+);
+
+/**
+ * Cette route protégée permet de supprimer un film
+ * @route DELETE /films/{id}
+ */
+router.delete(
+    "/supprimer-film/:id",
+    auth, 
+    [check("id").escape().trim().notEmpty().isString()],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+            const id = req.params.id;
+
+            await db.collection("Films").doc(id).delete();
+
+            res.status(200).json({ message: `Film avec l'ID ${id} supprimé` });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    }
+);
+
+module.exports = router;
---
+++ routes/utilisateurs.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("../middlewares/auth.js");
+const { check, validationResult } = require("express-validator");
+
+
+
+// Route pour l'inscription d'un nouvel utilisateur
+router.post(
+    "/inscription",
+    [
+        check("courriel").escape().trim().notEmpty().isEmail().normalizeEmail(),
+        check("mdp").escape().trim().notEmpty().isLength({ min: 8, max: 20 }).isStrongPassword({
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        }),
+    ],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+
+            const { mdp, courriel } = req.body;
+            const docRef = await db.collection("utilisateurs").where("courriel", "==", courriel).get();
+            const utilisateurs = [];
+
+            docRef.forEach((doc) => {
+                utilisateurs.push({ id: doc.id, ...doc.data() });
+            });
+
+            if (utilisateurs.length > 0) {
+                res.statusCode = 400;
+                res.json({ message: "Courriel déjà utilisé" });
+            } else {
+                const hash = await bcrypt.hash(mdp, 10);
+                const user = { courriel, mdp: hash };
+                const doc = await db.collection("utilisateurs").add(user);
+                const token = genererToken(doc.id);
+                res.json({ token });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    }
+);
+
+// Route pour la connexion d'un utilisateur
+router.post(
+    "/connexion",
+    [
+        check("mdp").escape().trim().notEmpty().isString(),
+        check("courriel").escape().trim().notEmpty().isEmail().normalizeEmail(),
+    ],
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+            if (!validation.isEmpty()) {
+                return res.status(400).json({ message: "Données invalides" });
+            }
+
+            const { mdp, courriel } = req.body;
+            const docRef = await db.collection("utilisateurs").where("courriel", "==", courriel).get();
+            if (docRef.empty) {
+                return res.status(400).json({ message: "Le courriel n'existe pas" });
+            }
+
+            const utilisateur = docRef.docs[0].data();
+            const resultatConnexion = await bcrypt.compare(mdp, utilisateur.mdp);
+
+            if (resultatConnexion) {
+                const token = genererToken(utilisateur.id);
+                res.json({ token });
+            } else {
+                res.status(400).json({ message: "Mot de passe incorrect" });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).send("Une erreur s'est produite lors de la connexion.");
+        }
+    }
+);
+
+// Fonction pour générer un token JWT
+const genererToken = function (id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+module.exports = router;
+
